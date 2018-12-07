c002b16ea16d6c11c08af103ff5bd98516ae8061
Raikume <nakurakanra@hotmail.com>
2018-12-07 07:34:42 +0000

    testing if comments in node don't work

--- server/server.js
+++ server/server.js
@@ -1,16 +1,6 @@
 const http = require('http');
 const port = 8080;
 
-
-/*Put page resolution here
-var pages = function (req, res){
-  let html = require('./public/index.html');
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write(html);
-  res.end();
-}
-*/
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
