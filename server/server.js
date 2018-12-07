65a9f2e5aef41e5efdc44c4296dadf1cf789ea32
Raikume <nakurakanra@hotmail.com>
2018-12-07 07:37:16 +0000

    comment test 2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const port = 8080;
-//
+
+
+/*Other comment*/
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
