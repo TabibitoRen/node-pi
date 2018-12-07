a6c509b4dde118516bfa01543cf278ad657bf674
Raikume <nakurakanra@hotmail.com>
2018-12-07 07:28:28 +0000

    undo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const port = 8080;
 
-//Put page resolution here
+
+/*Put page resolution here
 var pages = function (req, res){
   let html = require('./public/index.html');
   res.statusCode = 200;
@@ -9,8 +10,12 @@ var pages = function (req, res){
   res.write(html);
   res.end();
 }
-//
-const server = http.createServer(pages);
+*/
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
 
 server.listen(port, () => {
   console.log(`Server running at ${port}`);
