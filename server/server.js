9e7aa405f5a22c6190739c1f2102d5f1738c7738
Raikume <nakurakanra@hotmail.com>
2018-12-07 07:22:34 +0000

    figuring out how the paging works

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const port = 8080;
+
+//Put page resolution here
+var pages = function (req, res){
+  let html = require('./public/index.html');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(html);
+  res.end();
+}
+//
+const server = http.createServer(pages);
+
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
