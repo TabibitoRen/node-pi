81252e626b6a7eaef4e4d0e2bcb301c9b93ced39
Raikume <nakurakanra@hotmail.com>
2018-12-23 15:25:44 +0000

    reading data from readFileSync

--- server/util.js
+++ server/util.js
@@ -3,14 +3,17 @@ exports.navi = function(path){
   let url =  require("url");
   this.paths =  function(){
     let public = fs.readdirSync(__dirname+path);
+    let publicData = {};
+    for(var fileName in public ){
+      publicData[fileName] = fs.readFileSync(__dirname+path"/"+fileName);
+    }
     let index = "index.html";
     return {
       pages:function(req,res){
         let reqUrl = url.parse(req.url,true); //favicon is always requested on top of the url
-        console.log(public);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.write("<html><head><title>Testing</title></head><body>Let's hope this works: the requested url is </body></html>");
+        res.write(publicData['index.html']);
         res.end();
       }
     };
