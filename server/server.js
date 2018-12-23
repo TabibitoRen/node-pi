fc879c15f10b6865dcfa5e4a231d839f75b05f79
Raikume <nakurakanra@hotmail.com>
2018-12-23 15:43:10 +0000

    completing navi

--- server/server.js
+++ server/server.js
@@ -3,22 +3,6 @@ const url = require('url');
 const util = require('./util.js');
 
 var navi = new util.navi("/public");
-/*
-//Put page resolution here
-var pages = function (req, res){
-  //let html = require('./public/index.html'); I need this to be read as string
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write("<html><head><title>Test</title></head><body>body test</body></html>");
-  res.end();
-}
-/*
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-*/
 const server = http.createServer(navi.paths.pages);
 
 server.listen(8080, () => {
--- server/util.js
+++ server/util.js
@@ -5,16 +5,16 @@ exports.navi = function(path){
     let public = fs.readdirSync(__dirname+path);
     let publicData = {};
     for(var fileName in public ){
-      publicData[public[fileName]] = fs.readFileSync(__dirname+path+"/"+public[fileName]);
+      publicData["/"+public[fileName]] = fs.readFileSync(__dirname+path+"/"+public[fileName]);
     }
-    let index = "index.html";
+    publicData["/"] = fs.readFileSync(__dirname+path+"/index.html");
     return {
       pages:function(req,res){
         res.setHeader('Content-Type', 'text/html');
         let reqUrl = url.parse(req.url,true); //favicon is always requested on top of the url
         try{
           res.statusCode = 200;
-          res.write(publicData[reqUrl.path.substring(1)]);
+          res.write(publicData[reqUrl.path]);
           res.end();
         }
         catch(err){
